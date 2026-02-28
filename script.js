1c14194057ca79455a75b65fa73afe0c48ac1053
Daniel Joseph M <danieldaniel14042020@gmail.com>
2026-02-28 18:43:20 +0530

    Added smooth task animation

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-// Add Task Function
 function addTask() {
   const input = document.getElementById("taskInput");
-  const taskText = input.value.trim();
+  const taskText = input.value;
 
-  if (taskText === "") {
-    alert("Please enter a task");
-    return;
-  }
-  li.onclick = function () {
-  li.style.textDecoration = "line-through";
-};
+  if (taskText === "") return;
+
   const li = document.createElement("li");
   li.textContent = taskText;
 
-  // Add delete button
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "❌";
-  deleteBtn.style.marginLeft = "10px";
-
-  deleteBtn.onclick = function () {
-    li.remove();
-  };
-
-  li.appendChild(deleteBtn);
   document.getElementById("taskList").appendChild(li);
 
   input.value = "";
